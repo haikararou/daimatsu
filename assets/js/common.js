b4b854a346f52d1768a9257a144a126e81f338bd
haikararou <chimiriho7@yahoo.co.jp>
2023-06-19 16:24:31 +0900

    20230619-001

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,49 +1,50 @@
-// const headerShadow = document.querySelector(".p-header__shadow");
-// const headerLogo = document.querySelector(".p-header__logo");
-// const headerNav = document.querySelector(".p-header__nav-container");
-// const body = document.querySelector(".l-body");
-// const header = document.querySelector(".p-header");
-// const menuButton = document.querySelector(".p-header__menu-button");
-// const menuDesign = document.querySelector(".p-header__menu-design");
-// const menuContainer = document.querySelector(".p-header__menu-container");
-// const menuOuter = document.querySelector(".p-header__menu-outer");
+//const headerShadow = document.querySelector(".p-header__shadow");
+const headerLogo = document.querySelector(".p-header__logo");
+const headerNav = document.querySelector(".p-header__nav-container");
+const body = document.querySelector(".l-body");
+const header = document.querySelector(".p-header");
+const menuButton = document.querySelector(".p-header__menu-button");
+const menuDesign = document.querySelector(".p-header__menu-design");
+//const menuContainer = document.querySelector(".p-header__menu-container");
+//const menuOuter = document.querySelector(".p-header__menu-outer");
 
-// const vr = 1280; //タブレット表示に切り替える画面幅
-// let logoHeight = headerLogo.clientHeight;
-// let headerNavCoordinate = headerNav.getBoundingClientRect();
-// let headerTop = headerNavCoordinate.top;
-// let tempTop = 0;
-// let tempHeight = 0;
+//const vr = 1280; //タブレット表示に切り替える画面幅
+const tabl = 960;
+let logoHeight = headerLogo.clientHeight;
+let headerNavCoordinate = headerNav.getBoundingClientRect();
+let headerTop = headerNavCoordinate.top;
+let tempTop = 0;
+let tempHeight = 0;
 /*------------------------------------------------------------------
 ヘッダー制御
 /*------------------------------------------------------------------*/
-// const headerScroll = () => {
-//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   let windowWidth = window.innerWidth;
-//   tempHeight = logoHeight - scrollTop;
-//   tempTop = headerTop - scrollTop;
-//   if (scrollTop <= headerTop) {
-//     header.style.height = tempHeight + "px";
-//     headerLogo.style.height = tempHeight + "px";
-//     headerNav.style.top = tempTop + "px";
-//     headerShadow.style.height = tempHeight + "px";
-//   } else if (scrollTop > headerTop && tempTop != 0) {
-//     if (windowWidth >= vr) {
-//       tempHeight = 100;
-//       tempTop = 0;
-//     } else {
-//       tempHeight = 60;
-//       tempTop = 0;
-//     }
-//     headerShadow.style.height = tempHeight + "px";
-//     headerLogo.style.height = tempHeight + "px";
-//     headerNav.style.top = tempTop + "px";
-//   }
-// };
+const headerScroll = () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let windowWidth = window.innerWidth;
+  tempHeight = logoHeight - scrollTop;
+  tempTop = headerTop - scrollTop;
+  if (scrollTop <= headerTop) {
+    header.style.height = tempHeight + "px";
+    headerLogo.style.height = tempHeight + "px";
+    headerNav.style.top = tempTop + "px";
+    //headerShadow.style.height = tempHeight + "px";
+  } else if (scrollTop > headerTop && tempTop != 0) {
+    if (windowWidth >= tabl) {
+      tempHeight = 100;
+      tempTop = 0;
+    } else {
+      tempHeight = 60;
+      tempTop = 0;
+    }
+    //headerShadow.style.height = tempHeight + "px";
+    headerLogo.style.height = tempHeight + "px";
+    headerNav.style.top = tempTop + "px";
+  }
+};
 
-// window.addEventListener("scroll", function () {
-//   headerScroll();
-// });
+window.addEventListener("scroll", function () {
+  headerScroll();
+});
 
 /*------------------------------------------------------------------
 メニュー開閉
@@ -65,7 +66,7 @@
 // window.onload = function () {
 //   headerScroll();
 //   let windowWidth = window.innerWidth;
-//   if (windowWidth < vr) {
+//   if (windowWidth < tabl) {
 //     headerShadow.classList.remove("active");
 //   }
 // };
@@ -74,29 +75,29 @@
 リサイズ処理
 /*------------------------------------------------------------------*/
 // const resizeEvent = () => {
-//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   let windowWidth = window.innerWidth;
-//   headerScroll();
-//   if (windowWidth >= vr) {
+//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     let windowWidth = window.innerWidth;
+//     headerScroll();
+//     if (windowWidth >= tabl) {
 //     headerTop = 60;
 //     logoHeight = 160;
 //     //メニューを開いていたら閉じる
-//     headerShadow.classList.add("active");
-//     menuOuter.classList.remove("active");
-//     header.classList.remove("active");
-//     menuButton.classList.remove("active");
-//     menuDesign.classList.remove("active");
-//     menuContainer.classList.remove("active");
-//     body.classList.remove("active");
+//     //headerShadow.classList.add("active");
+//     // menuOuter.classList.remove("active");
+//     // header.classList.remove("active");
+//     // menuButton.classList.remove("active");
+//     // menuDesign.classList.remove("active");
+//     //menuContainer.classList.remove("active");
+//     //body.classList.remove("active");
 
 //     if (scrollTop < 60) {
-//       headerNav.style.top = 60 + "px";
+//         headerNav.style.top = 60 + "px";
 //     }
-//   } else if (windowWidth < vr) {
-//     logoHeight = 60;
-//     headerShadow.classList.remove("active");
-//     headerNav.style.top = 0 + "px";
-//   }
+//     } else if (windowWidth < tabl) {
+//         logoHeight = 60;
+//         //headerShadow.classList.remove("active");
+//         headerNav.style.top = 0 + "px";
+//     }
 // };
 // window.addEventListener("resize", resizeEvent);
 
@@ -175,3 +176,23 @@ function drowerChange(bl) {
         );
     }
 }
+
+/*------------------------------------------------------------------
+リサイズ処理
+/*------------------------------------------------------------------*/
+var $win = $(window);
+$win.on('load resize', function() {
+    if (window.matchMedia('(max-width:960px)').matches) {
+        // SPの処理
+    } else {
+        // PCの処理
+        $(function() {
+            $(body).addClass('pc');
+            $(body).removeClass('sp');
+            $('.p-header__nav').css({'padding-top':'0px','opacity':'1','display':'flex'});
+            $('.p-header__nav-container').css({'opacity':'1'});
+            $('.p-header').removeClass('active');
+            $('.p-header__menu-design').removeClass('active');
+        });
+    }
+});
