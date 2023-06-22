82ddf77471fea4c6e9e1473e0e2fc43ab32c72ea
haikararou <chimiriho7@yahoo.co.jp>
2023-06-22 11:39:28 +0900

    20230622-001

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,20 @@
-//const headerShadow = document.querySelector(".p-header__shadow");
-const headerLogo = document.querySelector(".p-header__logo");
-const headerNav = document.querySelector(".p-header__nav-container");
-const body = document.querySelector(".l-body");
-const header = document.querySelector(".p-header");
-const menuButton = document.querySelector(".p-header__menu-button");
-const menuDesign = document.querySelector(".p-header__menu-design");
-//const menuContainer = document.querySelector(".p-header__menu-container");
-//const menuOuter = document.querySelector(".p-header__menu-outer");
+// const headerShadow = document.querySelector(".p-header__shadow");
+// const headerLogo = document.querySelector(".p-header__logo");
+// const headerNav = document.querySelector(".p-header__nav-container");
+// const body = document.querySelector(".l-body");
+// const header = document.querySelector(".p-header");
+// const menuButton = document.querySelector(".p-header__menu-button");
+// const menuDesign = document.querySelector(".p-header__burger---design");
+// const menuContainer = document.querySelector(".p-header__menu-container");
+// const menuOuter = document.querySelector(".p-header__menu-outer");
 
 //const vr = 1280; //タブレット表示に切り替える画面幅
 const tabl = 960;
-let logoHeight = headerLogo.clientHeight;
-let headerNavCoordinate = headerNav.getBoundingClientRect();
-let headerTop = headerNavCoordinate.top;
-let tempTop = 0;
-let tempHeight = 0;
+// let logoHeight = headerLogo.clientHeight;
+// let headerNavCoordinate = headerNav.getBoundingClientRect();
+// let headerTop = headerNavCoordinate.top;
+// let tempTop = 0;
+// let tempHeight = 0;
 /*------------------------------------------------------------------
 ヘッダー制御
 /*------------------------------------------------------------------*/
@@ -27,16 +27,16 @@ let tempHeight = 0;
 //     header.style.height = tempHeight + "px";
 //     headerLogo.style.height = tempHeight + "px";
 //     headerNav.style.top = tempTop + "px";
-//     //headerShadow.style.height = tempHeight + "px";
+//     headerShadow.style.height = tempHeight + "px";
 //   } else if (scrollTop > headerTop && tempTop != 0) {
 //     if (windowWidth >= tabl) {
-//       //tempHeight = 100;
+//       tempHeight = 30;
 //       tempTop = 0;
 //     } else {
-//       //tempHeight = 60;
+//       tempHeight = 12;
 //       tempTop = 0;
 //     }
-//     //headerShadow.style.height = tempHeight + "px";
+//     headerShadow.style.height = tempHeight + "px";
 //     headerLogo.style.height = tempHeight + "px";
 //     headerNav.style.top = tempTop + "px";
 //   }
@@ -70,6 +70,38 @@ let tempHeight = 0;
 //     headerShadow.classList.remove("active");
 //   }
 // };
+$(function() {
+    let windowWidth = window.innerWidth;
+    $(window).on('load', function(){
+    if (window.matchMedia('(max-width:960px)').matches) {
+    //if (windowWidth < tabl) {
+        // $('.l-body').addClass('sp');
+        // $('.l-body').removeClass('pc');
+    } else {
+        // $('.l-body').removeClass('sp');
+        // $('.l-body').addClass('pc');
+    }
+    });
+    //var topBtn = $('#page-top');
+    //topBtn.hide();
+    //スクロールが120に達したらボタン表示
+    $(window).on('load scroll', function(){
+    //$(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            //topBtn.fadeIn();
+            $('.l-body').addClass('scroll');
+            $('.p-header__nav-list.nav-list01').css('opacity','0');
+            $('.p-header__nav-list.nav-list02 a').css('opacity','0');
+        } else {
+            //topBtn.fadeOut();
+            $('.l-body').removeClass('scroll');
+            $('.l-body').removeClass('open');
+            modalopen = false;
+            $('.p-header__nav-list.nav-list01').css('opacity','1');
+            $('.p-header__nav-list.nav-list02 a').css('opacity','1');
+        }
+    });
+});
 
 /*------------------------------------------------------------------
 リサイズ処理
@@ -106,7 +138,7 @@ let tempHeight = 0;
 
 /*ドロワーメニュー --------------------------------------------------------------------------------------------*/
 var modalopen = false;
-$('.p-header__menu-button').click(function (e) {
+$('.p-header__burger').click(function (e) {
     if (modalopen == false) {
         drowerChange('view');
     } else if (modalopen == true) {
@@ -119,11 +151,10 @@ $('.p-header__menu-button').click(function (e) {
 function drowerChange(bl) {
     if (bl == 'view') {
         modalopen = true;
-        $('.p-header').addClass('active');
-        $('.p-header__menu-design').addClass('active');
+        $('.l-body').addClass('open');
         TweenMax.fromTo(
             $('.p-header__nav-container'),
-            0.8,
+            0.4,
             {},
             {
                 opacity: '1',
@@ -135,9 +166,9 @@ function drowerChange(bl) {
         TweenMax.fromTo(
             $('.p-header__nav'),
             0.8,
-            { 'padding-top': '40px' },
+            { 'padding-top': '0px' },
             {
-                'padding-top': '20px',
+                'padding-top': '0px',
                 opacity: '1',
                 display: 'block',
                 delay: 0.6,
@@ -145,14 +176,66 @@ function drowerChange(bl) {
                 onComplete: function () {},
             }
         );
+        TweenMax.fromTo(
+            $('.p-header__nav-list.nav-list01'),
+            0.6,
+            {},
+            {
+                opacity: '1',
+                //display: 'block',
+                delay: 0.3,
+                ease: Power2.easeOut,
+                onComplete: function () {},
+            }
+        );
+        TweenMax.fromTo(
+            $('.p-header__nav-list.nav-list02 a'),
+            0.6,
+            { opacity: '0' },
+            {
+                opacity: '1',
+                //display: 'block',
+                delay: 0.3,
+                ease: Power2.easeOut,
+                onComplete: function () {},
+            }
+        );
+
+        background: rgba(17,67,40,1);
+
     } else if (bl == 'hiden') {
         modalopen = false;
+        //$('.l-body').removeClass('open');
+        TweenMax.fromTo(
+            $('.p-header__nav-list.nav-list02 a'),
+            0.6,
+            { opacity: '1' },
+            {
+                opacity: '0',
+                delay: 0,
+                ease: Power2.easeOut,
+                onComplete: function () {},
+            }
+        );
+        TweenMax.fromTo(
+            $('.p-header__nav-list.nav-list01'),
+            0.2,
+            { opacity: '0' },
+            {
+                //opacity: '0',
+                delay: 0.2,
+                ease: Power2.easeOut,
+                // onComplete: function () {
+                //     $('.l-body').removeClass('open');
+                // },
+            }
+        );
         TweenMax.fromTo(
             $('.p-header__nav'),
             0.3,
-            { 'padding-top': '20px' },
+            { 'padding-top': '0px' },
             {
-                'padding-top': '40px',
+                'padding-top': '0px',
                 opacity: '0',
                 display: 'none',
                 delay: 0,
@@ -165,12 +248,11 @@ function drowerChange(bl) {
             0.4,
             { opacity: '1' },
             {
-                opacity: '0',
+                opacity: '1',
                 delay: 0.2,
                 ease: Power2.easeOut,
                 onComplete: function () {
-                    $('.p-header').removeClass('active');
-                    $('.p-header__menu-design').removeClass('active');
+                    $('.l-body').removeClass('open');
                 },
             }
         );
@@ -180,19 +262,46 @@ function drowerChange(bl) {
 /*------------------------------------------------------------------
 リサイズ処理
 /*------------------------------------------------------------------*/
-var $win = $(window);
-$win.on('load resize', function() {
-    if (window.matchMedia('(max-width:960px)').matches) {
-        // SPの処理
+//var $win = $(window);
+// $win.on('load resize', function() {
+//     if (window.matchMedia('(max-width:960px)').matches) {
+//         // SPの処理
+//         $(function() {
+//             $('.l-body').removeClass('open');
+//         });
+//     } else {
+//         // PCの処理
+//         $(function() {
+//             $('.l-body').removeClass('open');
+//             //$(body).addClass('pc');
+//             // $(body).addClass('pc');
+//             // $(body).removeClass('sp');
+//             // $('.p-header__nav').css({'padding-top':'0px','opacity':'1','display':'flex'});
+//             // $('.p-header__nav-container').css({'opacity':'1'});
+//             // $('.p-header').removeClass('active');
+//             // $('.p-header__burger---design').removeClass('active');
+//         });
+//     }
+// });
+
+const mediaQueryList = window.matchMedia('(min-width: 960px)');
+const listener = (event) => {
+    if (event.matches) {
+        // 768px以上
+        console.log('PC用ブレークポイント用処理');
+        $('.l-body').removeClass('open');
+        $('.l-body').removeClass('sp');
+        $('.l-body').addClass('pc');
     } else {
-        // PCの処理
-        $(function() {
-            $(body).addClass('pc');
-            $(body).removeClass('sp');
-            $('.p-header__nav').css({'padding-top':'0px','opacity':'1','display':'flex'});
-            $('.p-header__nav-container').css({'opacity':'1'});
-            $('.p-header').removeClass('active');
-            $('.p-header__menu-design').removeClass('active');
-        });
+        // 768px未満
+        console.log('SP用ブレークポイント用処理');
+        $('.l-body').removeClass('open');
+        $('.l-body').addClass('sp');
+        $('.l-body').removeClass('pc');
     }
-});
+};
+mediaQueryList.addEventListener("change", listener);
+// 初期化処理
+listener(mediaQueryList);
+
+
